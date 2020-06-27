965e041e31734c5ccd6e7d59b62d4f20970ddcd9
Jair Barcasnegras <jabares2010@gmail.com>
2020-06-27 02:05:34 -0500

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,4 @@ router.get('/',(req,res)=>{
     res.render('index.html',{title: '¿Por qué estamos juntos?'});
 });
 
-router.get('/amor',(req,res)=>{
-    res.render('amor.html',{title: 'Amarte menos es imposible'});
-});
-
-router.get('/felicidad',(req,res)=>{
-    res.render('felicidad.html',{title: 'Nos merecemos lo mejor'});
-});
-
-
-router.get('/futuro',(req,res)=>{
-    res.render('futuro.html',{title: 'Nos merecemos lo mejor'});
-});
- module.exports = router;
+module.exports = router;
